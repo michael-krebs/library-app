41321bcd0df2815991de6ab600a3295b09ad0bb3
Michael Krebs <vault742@gmail.com>
2015-10-03 21:57:50 -0400

    i'm writing javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
 	$('#button').click(function(event) {
+		parseXML();
 	})
+
+	var parseXML = function() {
+		var titleArr = []
+		titleArr.append($('#inputBook1').val());
+		titleArr.append($('#inputBook2').val());
+		titleArr.append($('#inputBook3').val());
+		console.log(titleArr);
+		for (var i = 0; i < 3; i++) {
+			$.get("http://www.librarything.com/api/thingTitle/" + titleArr[i], function(data) {
+				
+			})
+		}
+		
+	}
 });
